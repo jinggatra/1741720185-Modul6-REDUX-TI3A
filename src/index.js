96c6aa2582418a4bd8731f8b0e5fa36c40b9b3d2
jinggatra <wmkbisa@gmail.com>
2020-03-19 10:02:41 +0700

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom'; import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux' 
+import MainReducer from './reducers/MainReducer'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>
+    , document.getElementById('root')); registerServiceWorker();
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
